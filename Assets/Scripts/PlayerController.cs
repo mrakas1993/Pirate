249b06b6d677fae8dc75a5173d737303a65f379f
andreymakeenko <mrakas1993@gmail.com>
2022-10-10 20:47:02 +0300

    edit player Settings

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,9 +6,17 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
     private float horizontalMove = 0f;
-    public float speed = 1f;
+    
     private bool facingRight = true;
+    [Header("Player Settings")]
+    [Range(0,15f)]public float speed = 1f;
     public float jumpForce = 8f;
+    [Space]
+    [Header("Ground Check Settings")]
+    public bool isGrounded = false;
+    [Range(-2f, 2f)] public float checkGroundOffsetY = -1.1f;
+    [Range(0, 5f)] public float checkGroundRadius = 0.3f;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -26,15 +34,17 @@ public class PlayerController : MonoBehaviour
         {
             Flip();
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+
         }
     }
     private void FixedUpdate()
     {
         Vector2 targetVelocity = new Vector2(horizontalMove * 2f, rb.velocity.y);
         rb.velocity = targetVelocity;
+        CheckGround();
     }
     private void Flip()
     {
@@ -42,5 +52,18 @@ public class PlayerController : MonoBehaviour
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;
         transform.localScale=theScale;
+
+    }
+    private void CheckGround()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(new Vector2(transform.position.x, transform.position.y + checkGroundOffsetY), checkGroundRadius);
+        if (colliders.Length > 1)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
     }
 }
